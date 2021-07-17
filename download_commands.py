69c98cb0e99550462c16e69ff322dac07b078d9f
ddlinz <ddlinz@gmail.com>
2021-07-17 13:38:40 -0700

    added basic download script

---
+++ download_commands.py
@@ -0,0 +1,40 @@
+from __future__ import unicode_literals
+import youtube_dl as yt
+
+
+class DownloadCommands:
+
+    default_download_directory = "None"
+    ydl_opts = {}
+    downloader =yt.YoutubeDL(ydl_opts)    
+
+    ##
+    #
+    # 
+    def downloadPlayList(self, playlist_url, directory="default") : 
+        print('downloading playlist')
+
+        if directory == "default": 
+            print('no directory specified, using default: ' + self.default_download_directory)
+
+        self.downloader.download([playlist_url])
+
+
+        return
+
+
+def main():
+    print("running tests for downloading basics")
+    dl_cmd = DownloadCommands()
+    dl_cmd.default_download_directory = '/home/dave/radio_directory'
+    print(dl_cmd.default_download_directory)
+
+    playlist_url_input = "https://www.youtube.com/playlist?list=PLRbcQXWEJAJpVlnuqiqqZ27FmWlxka62l"
+    ##  playlist_url_input = 'https://www.youtube.com/watch?v=dP15zlyra3c'
+    
+
+    dl_cmd.downloadPlayList(playlist_url=playlist_url_input)
+
+if __name__ == "__main__":
+    main()
+
