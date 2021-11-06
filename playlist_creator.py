f853596f19e97de24ef9c6bdc78ba10232739fe3
ddlinz <ddlinz@gmail.com>
2021-11-06 01:08:59 -0400

    alternative changes from linux machine

--- playlist_creator.py
+++ playlist_creator.py
@@ -1,14 +1,21 @@
 #from __future__ import unicode_literals
-import youtube_dl as yt
+#import youtube_dl as yt
 import vlc
 import time
 import os
 
+from mutagen.mp3 import MP3  
+from mutagen.easyid3 import EasyID3  
+import mutagen.id3  
+from mutagen.id3 import ID3, TIT2, TIT3, TALB, TPE1, TRCK, TYER  
+  
+import glob  
 
 ## copied this code from t
 class VLC:
     def __init__(self):
-        self.Player = vlc.Instance('--loop')
+       self.Player = vlc.Instance('--loop')
+       ## self.Player = vlc.Instance()
     def addPlaylistFromDir(self, path):
         self.mediaList = self.Player.media_list_new()
         file_names = os.listdir(path)
@@ -16,11 +23,12 @@ class VLC:
         for s in file_names:
             if s.split(".",1)[1] == "mp3" : 
                 songs.append(s)
-            self.mediaList.add_media(self.Player.media_new(os.path.join(path,s)))
+                self.mediaList.add_media(self.Player.media_new(os.path.join(path,s)))
         self.listPlayer = self.Player.media_list_player_new()
         self.listPlayer.set_media_list(self.mediaList)
     def play(self):
         self.listPlayer.play()
+        # self.mediaList.play()
     def next(self):
         self.listPlayer.next()
     def pause(self):
@@ -29,6 +37,11 @@ class VLC:
         self.listPlayer.previous()
     def stop(self):
         self.listPlayer.stop()
+    def printPlaylist(self): 
+        for media_instance in self.mediaList: 
+            media_instance.parse()
+            print(media_instance.get_meta(12))
+            print(media_instance.get_duration())
 
 # we will document the playlist entries based on tags later ##
 class PlaylistEntry: 
@@ -41,9 +54,11 @@ class MediaPlaylistManager:
     ## ##
     candidateList = [] 
 
+    ## ##
     def __init__(self) : 
         self.player = VLC()
 
+    ## ##
     def pullCandidateListsAndTags(self):
         return 
 
@@ -57,13 +72,22 @@ class MediaPlaylistManager:
         return
     
     def AddAllMp3FromDirectory(self, input_directory) : 
-
-
         return
     
     def PlayAllFromDirectory(self, input_directory) : 
-        
 
+        print('playing everything from the directory {}'.format(input_directory))
+        self.player.addPlaylistFromDir(input_directory)
+
+
+        self.player.printPlaylist()
+        self.player.play()
+
+        time.sleep(20)
+        self.player.next()
+        self.player.play()
+        time.sleep(20)
+        print('playing')
 
         return
 
@@ -71,9 +95,8 @@ class MediaPlaylistManager:
 ## main test function for early functionality 
 def main():
     playlist = MediaPlaylistManager() 
-    ## dl_cmd.downloadPlayList(playlist_url=playlist_url_input)
-
     play_directory = '/home/dave/radio_directory/test/'
+    play_directory = '/home/dave/Dropbox/BloggingFileFolder/Media/Audio/Music-Jazz'
     playlist.PlayAllFromDirectory(play_directory)
 
 if __name__ == "__main__":
